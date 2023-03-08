a74d0a8a5c6c31cf4df8612d25a7fb94fdfc20ea
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-03-08 17:05:12 +0800

    Add ProductCard.tsx

---
+++ react/src/components/ProductCard.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Typography,
+} from "@material-tailwind/react";
+
+type ProductCardProps = {
+  title: string;
+  price: string;
+  imageUrl: string;
+};
+
+function ProductCard({ title, price, imageUrl }: ProductCardProps) {
+  return (
+    <Card className="w-52" shadow={false}>
+      <CardHeader floated={false} shadow={false}>
+        <img src={imageUrl} alt={`Image of product ${title}`} />
+      </CardHeader>
+      <CardBody>
+        <Typography variant="h6">{title}</Typography>
+        <Typography variant="h6">{price}</Typography>
+      </CardBody>
+    </Card>
+  );
+}
+
+export default ProductCard;
