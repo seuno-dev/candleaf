f2edccf851f702fb0e796dbc0a65b5c974f289fe
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-03-12 15:07:35 +0800

    Add a mechanism to log user out if the refresh token has expired

--- react/src/services/api.ts
+++ react/src/services/api.ts
@@ -1,106 +1,6 @@
-import axios from "axios";
+import { login, logout, instance, REFRESH_KEY } from "./client";
 
-const BASE_URL = "http://127.0.0.1:8000/";
-
-const REFRESH_KEY = "refresh";
-const ACCESS_KEY = "access";
-
-let isRefreshing = false;
-type refreshQueueType = {
-  resolve: () => void;
-  reject: (reason?: unknown) => void;
-};
-const refreshQueue: refreshQueueType[] = [];
-
-const instance = axios.create({
-  baseURL: BASE_URL,
-});
-
-instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem(ACCESS_KEY);
-  if (token) {
-    config.headers["Authorization"] = `JWT ${token}`;
-  }
-  return config;
-});
-
-instance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    const originalRequest = error.config;
-    const refreshToken = localStorage.getItem(REFRESH_KEY);
-
-    if (
-      error.response &&
-      error.response.status === 401 &&
-      error.config &&
-      !error.config.__isRetryRequest &&
-      refreshToken
-    ) {
-      originalRequest._retry = true;
-      if (!isRefreshing) {
-        console.log("refreshing token!");
-        isRefreshing = true;
-
-        const response = await fetch(`${BASE_URL}auth/jwt/refresh/`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            refresh: refreshToken,
-          }),
-        });
-        const data = await response.json();
-        const newToken = data.access;
-        localStorage.setItem(ACCESS_KEY, newToken);
-        refreshQueue.forEach((request) => request.resolve());
-        console.log("token refreshed!");
-        isRefreshing = false;
-        return instance(originalRequest);
-      } else {
-        return new Promise<void>((resolve, reject) => {
-          refreshQueue.push({ resolve, reject });
-        })
-          .then(() => {
-            return instance(originalRequest);
-          })
-          .catch((error) => {
-            return Promise.reject(error);
-          });
-      }
-    }
-    return Promise.reject(error);
-  }
-);
-
-export const login = async (username: string, password: string) => {
-  const response = await fetch(`${BASE_URL}auth/jwt/create/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ username, password }),
-  });
-  if (response.status === 401) {
-    throw new Error();
-  }
-  const data = await response.json();
-  const refreshToken = data.refresh;
-  const accessToken = data.access;
-
-  localStorage.setItem(REFRESH_KEY, refreshToken);
-  localStorage.setItem(ACCESS_KEY, accessToken);
-
-  return { refreshToken, accessToken };
-};
-
-export const logout = () => {
-  localStorage.removeItem(REFRESH_KEY);
-  localStorage.removeItem(ACCESS_KEY);
-};
+export { login, logout };
 
 export const getAuthenticationStatus = () => {
   return localStorage.getItem(REFRESH_KEY) !== null;
@@ -154,4 +54,22 @@ export const createCartItem = async (productId: number) => {
   } catch (e) {
     return false;
   }
+};
+
+type SimpleProduct = {
+  id: number;
+  title: string;
+  unit_price: number;
+};
+export type CartItemListResponse = {
+  id: number;
+  product: SimpleProduct;
+  quantity: number;
+  total_price: number;
+};
+export const retrieveCartItemList = async () => {
+  const response = await instance.get<CartItemListResponse[]>(
+    "/store/cart-items/"
+  );
+  return response.data;
 };
---
+++ react/src/services/client.ts
@@ -0,0 +1,108 @@
+import axios from "axios";
+
+export const REFRESH_KEY = "refresh";
+const ACCESS_KEY = "access";
+
+let isRefreshing = false;
+type refreshQueueType = {
+  resolve: () => void;
+  reject: (reason?: unknown) => void;
+};
+const refreshQueue: refreshQueueType[] = [];
+
+export const BASE_URL = "http://127.0.0.1:8000/";
+
+export const instance = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const login = async (username: string, password: string) => {
+  const response = await fetch(`${BASE_URL}auth/jwt/create/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  });
+  if (response.status === 401) {
+    throw new Error();
+  }
+  const data = await response.json();
+  const refreshToken = data.refresh;
+  const accessToken = data.access;
+
+  localStorage.setItem(REFRESH_KEY, refreshToken);
+  localStorage.setItem(ACCESS_KEY, accessToken);
+
+  return { refreshToken, accessToken };
+};
+
+export const logout = () => {
+  localStorage.removeItem(REFRESH_KEY);
+  localStorage.removeItem(ACCESS_KEY);
+};
+
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem(ACCESS_KEY);
+  if (token) {
+    config.headers["Authorization"] = `JWT ${token}`;
+  }
+  return config;
+});
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    const refreshToken = localStorage.getItem(REFRESH_KEY);
+
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      error.config &&
+      !error.config.__isRetryRequest &&
+      refreshToken
+    ) {
+      originalRequest._retry = true;
+      if (!isRefreshing) {
+        console.log("refreshing token!");
+        isRefreshing = true;
+
+        const response = await fetch(`${BASE_URL}auth/jwt/refresh/`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            refresh: refreshToken,
+          }),
+        });
+
+        if (response.status === 200) {
+          const data = await response.json();
+          const newToken = data.access;
+          localStorage.setItem(ACCESS_KEY, newToken);
+          refreshQueue.forEach((request) => request.resolve());
+          console.log("token refreshed!");
+          isRefreshing = false;
+          return instance(originalRequest);
+        } else if (response.status === 401) {
+          logout();
+        }
+      } else {
+        return new Promise<void>((resolve, reject) => {
+          refreshQueue.push({ resolve, reject });
+        })
+          .then(() => {
+            return instance(originalRequest);
+          })
+          .catch((error) => {
+            return Promise.reject(error);
+          });
+      }
+    }
+    return Promise.reject(error);
+  }
+);
