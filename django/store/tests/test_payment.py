903f61892efe571cd2764165f0138fd0041d9689
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-04-07 14:37:47 +0800

    Add TestStripeWebhook

--- django/store/tests/test_payment.py
+++ django/store/tests/test_payment.py
@@ -1,4 +1,5 @@
 import random
+from unittest import mock
 
 import pytest
 from django.core.cache import cache
@@ -52,3 +53,35 @@ class TestCreatePayment:
         response = client.post(create_payment_url, {'order_id': uninitialized_order.id})
 
         assert response.status_code == status.HTTP_409_CONFLICT
+
+
+@pytest.mark.django_db
+class TestStripeWebhook:
+    def test_stripe_webhook(self, authenticate_client, create_payment_url, api_client, uninitialized_order):
+        client = authenticate_client(user=uninitialized_order.customer.user)
+
+        # Send payment creation request so payment intent is created
+        client.post(create_payment_url, {'order_id': uninitialized_order.id})
+        uninitialized_order.refresh_from_db()
+
+        payload = "payload"
+        sig_header = "signature"
+
+        with mock.patch('stripe.Webhook.construct_event') as mock_construct_event:
+            mock_event = mock.Mock(
+                type='payment_intent.succeeded',
+                data=mock.Mock(
+                    object=mock.Mock(
+                        id=uninitialized_order.payment_intent_id,
+                        metadata=mock.Mock(
+                            order_id=uninitialized_order.id
+                        )
+                    )
+                )
+            )
+            mock_construct_event.return_value = mock_event
+
+            url = reverse('stripe-webhook')
+            response = api_client.post(url, data={'payload': payload}, HTTP_STRIPE_SIGNATURE=sig_header)
+
+            assert response.status_code == 200
