1ffe580c67aedac18acdbe6ecf273a870a74275f
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-04-13 05:35:53 +0800

    Add category filter component

--- react/src/features/Products/routes/ProductSearch.tsx
+++ react/src/features/Products/routes/ProductSearch.tsx
@@ -5,6 +5,7 @@ import Pagination from "../../../components/Elements/Pagination";
 import { formatCurrency } from "../../../utils/currency";
 import { retrieveProductList } from "../api";
 import { Product } from "../types";
+import FilterSideBar from "../components/FilterSideBar";
 
 function ProductSearch() {
   const [searchParams] = useSearchParams();
@@ -42,24 +43,29 @@ function ProductSearch() {
   }, [searchParams]);
 
   return (
-    <div className="container mx-auto mt-5 flex flex-col">
-      <ul className="flex flex-row flex-wrap gap-1">
-        {products.map((product) => (
-          <Link key={product.id} to={`/products/${product.slug}`}>
-            <ProductCard
-              title={product.title}
-              price={formatCurrency(product.unitPrice)}
-              imageUrl={
-                product.images.length > 0
-                  ? product.images[0].image
-                  : "logo512.png"
-              }
-            />
-          </Link>
-        ))}
-      </ul>
-      <div className="mt-5 flex flex-row justify-center">
-        <Pagination onPageChange={handlePageClick} pageCount={pageCount} />
+    <div className="container mx-auto mt-5 flex flex-row">
+      <div className="w-[480px]">
+        <FilterSideBar />
+      </div>
+      <div className="ml-5">
+        <ul className="flex flex-row flex-wrap gap-1">
+          {products.map((product) => (
+            <Link key={product.id} to={`/products/${product.slug}`}>
+              <ProductCard
+                title={product.title}
+                price={formatCurrency(product.unitPrice)}
+                imageUrl={
+                  product.images.length > 0
+                    ? product.images[0].image
+                    : "logo512.png"
+                }
+              />
+            </Link>
+          ))}
+        </ul>
+        <div className="mt-5 flex flex-row justify-center">
+          <Pagination onPageChange={handlePageClick} pageCount={pageCount} />
+        </div>
       </div>
     </div>
   );
