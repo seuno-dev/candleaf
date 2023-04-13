d3e560e9d72aa55714b7190963e021946b97b007
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-04-13 09:59:23 +0800

    Change filter fields for price

--- django/store/filters.py
+++ django/store/filters.py
@@ -6,9 +6,9 @@ from store import models
 class ProductFilter(django_filters.FilterSet):
     class Meta:
         model = models.Product
-        fields = ['title', 'category', 'unit_price']
+        fields = ['title', 'category', 'price_min', 'price_max']
 
     title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
     category = django_filters.NumberFilter(field_name='category')
-    unit_price_gt = django_filters.NumberFilter(field_name='unit_price', lookup_expr='gt')
-    unit_price_lt = django_filters.NumberFilter(field_name='unit_price', lookup_expr='lt')
+    price_min = django_filters.NumberFilter(field_name='unit_price', lookup_expr='gt')
+    price_max = django_filters.NumberFilter(field_name='unit_price', lookup_expr='lt')
