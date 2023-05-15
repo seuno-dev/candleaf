ac02faa5d6abca5a0edab1510e6131b9dd44700d
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-05-15 19:12:26 +0800

    fix: use baker to create reviews

--- django/dev/populate_products.py
+++ django/dev/populate_products.py
@@ -1,9 +1,11 @@
+import itertools
 import os
 import random
 from io import BytesIO
 
 from django.db import transaction
 from django.db.models.fields import files
+from model_bakery import baker
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'ShopZone.settings')
@@ -14,7 +16,7 @@ from django.utils.text import slugify
 import json
 
 django.setup()
-from store.models import Category, Product, ProductImage, Review
+from store.models import Category, Product, ProductImage, Review, OrderItem, Order
 
 
 def clean_and_create_category(title):
@@ -31,7 +33,6 @@ def download_temp_image(url):
     return file_name, files.File(fp)
 
 
-@transaction.atomic
 def populate():
     print("Populating categories...")
     cat_cache = {}
@@ -54,9 +55,25 @@ def populate():
             product_image.image.save(file_name, file)
             product_image.save()
 
-        for i in range(random.randint(0, 100)):
-            Review.objects.create(rating=random.randint(1, 5))
+        baker.make(Review, _quantity=random.randint(1, 10), order_item__product=product, rating=itertools.cycle([1, 3]),
+                   comment="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis blandit sem tristique, ultricies ex ac, congue ligula.")
+        baker.make(Review, _quantity=random.randint(1, 90), order_item__product=product, rating=itertools.cycle([4, 5]),
+                   comment="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis blandit sem tristique, ultricies ex ac, congue ligula.")
+
+
+def delete_current_data():
+    Category.objects.all().delete()
+    OrderItem.objects.all().delete()
+    Order.objects.all().delete()
+    Product.objects.all().delete()
+    Review.objects.all().delete()
+
+
+@transaction.atomic
+def main():
+    delete_current_data()
+    populate()
 
 
 if __name__ == '__main__':
-    populate()
+    main()
