cc698ff7e7def520154ac33b66df8e5e5d886727
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-05-15 14:56:33 +0800

    fix: add migrations

--- django/dev/populate_products.py
+++ django/dev/populate_products.py
@@ -8,10 +8,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 
 import requests
 import django
+from django.utils.text import slugify
 import json
 
 django.setup()
-from store.models import Category, Product, ProductImage
+from store.models import Category, Product, ProductImage, Review
 
 
 def clean_and_create_category(title):
@@ -24,7 +25,7 @@ def clean_and_create_category(title):
 def populate():
     print("Populating categories...")
     cat_responses = requests.get("https://dummyjson.com/products/categories")
-    for cat_title in cat_responses.text:
+    for cat_title in json.loads(cat_responses.text):
         print(cat_title)
         clean_and_create_category(cat_title)
 
@@ -32,17 +33,19 @@ def populate():
     product_responses = requests.get("https://dummyjson.com/products")
     for product_dict in json.loads(product_responses.text)['products']:
         print(product_dict['id'])
-        product = Product.objects.create(title=product_dict['title'], slug=product_dict['handle'],
+        category_qs = Category.objects.filter(title=product_dict['category'])
+        if category_qs.count() == 0:
+            continue
+        product = Product.objects.create(title=product_dict['title'], slug=slugify(product_dict['title']),
                                          description=product_dict['description'],
                                          unit_price=product_dict['price'],
-                                         category=Category.objects.get(title=product_dict['category']))
+                                         category=category_qs.first())
         for image in product_dict['images']:
             ProductImage.objects.create(product=product, image=image)
 
         for i in range(random.randint(100)):
+            Review.objects.create(rating=random.randint(1, 5))
 
 
-
-# Start execution here!
 if __name__ == '__main__':
     populate()
