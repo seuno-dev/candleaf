c95a8a3a2f51ed887fc2aaf7032277a9fdeaa3aa
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-05-16 12:38:37 +0800

    feat: add complete payment button on orders with awaiting payment status

--- react/src/features/Order/components/OrderCard.tsx
+++ react/src/features/Order/components/OrderCard.tsx
@@ -9,6 +9,7 @@ import {
 import { formatCurrency } from "../../../utils/currency";
 import { Order } from "../types";
 import OrderStatusLabel from "./OrderStatusLabel";
+import { useNavigate } from "react-router-dom";
 
 type OrderItemProps = {
   order: Order;
@@ -18,6 +19,7 @@ type OrderItemProps = {
 function OrderCard({ order, handleClickDetail }: OrderItemProps) {
   const items = order.items;
   const firstItem = items[0];
+  const navigate = useNavigate();
 
   return (
     <Card className="border-[0.5px] border-gray-200" shadow={false}>
@@ -48,18 +50,32 @@ function OrderCard({ order, handleClickDetail }: OrderItemProps) {
             </Typography>
           )}
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col items-end">
           <Typography variant="paragraph">Total price</Typography>
           <Typography variant="h6">
             {formatCurrency(order.totalPrice)}
           </Typography>
-          <Button
-            className="mt-5"
-            color="light-green"
-            onClick={() => handleClickDetail(order)}
-          >
-            See details
-          </Button>
+          <div className="flex flex-row gap-3">
+            <Button
+              className="mt-5"
+              color="light-green"
+              onClick={() => handleClickDetail(order)}
+            >
+              See details
+            </Button>
+            {order.status === "a" && (
+              <Button
+                className="mt-5"
+                color="light-green"
+                variant="outlined"
+                onClick={() =>
+                  navigate("/payment/", { state: { orderId: order.id } })
+                }
+              >
+                Complete payment
+              </Button>
+            )}
+          </div>
         </div>
       </CardBody>
     </Card>
