d7c8e37c9f3ad4d0f027d088641c3729d2cc5aea
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-05-17 14:01:49 +0800

    chore: remove an unused file

--- react/src/components/Elements/Error.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { isRouteErrorResponse, useRouteError } from "react-router-dom";
-import { Typography } from "@material-tailwind/react";
-
-
-
-export default Error;
