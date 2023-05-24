3a6e32a6f817f80e6f336954e486e3e2e22cc45a
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-05-24 09:36:39 +0800

    feat: simplify test

--- react/src/features/Auth/__tests__/Login.test.tsx
+++ react/src/features/Auth/__tests__/Login.test.tsx
@@ -1,4 +1,4 @@
-import { act, fireEvent, screen } from "@testing-library/react";
+import { fireEvent, screen } from "@testing-library/react";
 import { renderWithRoute } from "../../../test/utils";
 import userEvent from "@testing-library/user-event";
 import { password, username } from "../../../test/server/db/auth";
@@ -7,15 +7,9 @@ describe("Login", () => {
   it("should allow login and navigate to homepage", async () => {
     renderWithRoute("/auth/login");
 
-    const usernameInput = screen.getByTestId("username-input");
-    const passwordInput = screen.getByTestId("password-input");
-    const loginButton = screen.getByText("Login");
-
-    await act(async () => {
-      await userEvent.type(usernameInput, username);
-      await userEvent.type(passwordInput, password);
-      fireEvent.click(loginButton);
-    });
+    await userEvent.type(screen.getByTestId("username-input"), username);
+    await userEvent.type(screen.getByTestId("password-input"), password);
+    fireEvent.click(screen.getByText("Login"));
 
     // Brand on navbar indicates we are on Homepage
     expect(await screen.findByTestId("navbar-brand")).toBeDefined();
