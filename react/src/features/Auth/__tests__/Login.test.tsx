dde169b6c2869117d6d1a0c9b4cc54bd0a816e5f
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-05-24 09:43:48 +0800

    feat: add test for invalid credential

--- react/src/features/Auth/__tests__/Login.test.tsx
+++ react/src/features/Auth/__tests__/Login.test.tsx
@@ -3,9 +3,11 @@ import { renderWithRoute } from "../../../test/utils";
 import userEvent from "@testing-library/user-event";
 import { password, username } from "../../../test/server/db/auth";
 
+const renderLogin = () => renderWithRoute("/auth/login");
+
 describe("Login", () => {
   it("should allow login and navigate to homepage", async () => {
-    renderWithRoute("/auth/login");
+    renderLogin();
 
     await userEvent.type(screen.getByTestId("username-input"), username);
     await userEvent.type(screen.getByTestId("password-input"), password);
@@ -13,5 +15,17 @@ describe("Login", () => {
 
     // Brand on navbar indicates we are on Homepage
     expect(await screen.findByTestId("navbar-brand")).toBeDefined();
+  });
+
+  it("should show error message for an invalid credential", async () => {
+    renderLogin();
+
+    await userEvent.type(screen.getByTestId("username-input"), username + "i");
+    await userEvent.type(screen.getByTestId("password-input"), password + "i");
+    fireEvent.click(screen.getByText("Login"));
+
+    expect(
+      await screen.findByText("Invalid username or password")
+    ).toBeDefined();
   });
 });
