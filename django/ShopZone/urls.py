351c544c042853326de965eaeb61e062804cb584
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-06-06 16:05:11 +0800

    fix: remove wrong urls

--- django/ShopZone/urls.py
+++ django/ShopZone/urls.py
@@ -22,7 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
-    path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 ]
 
