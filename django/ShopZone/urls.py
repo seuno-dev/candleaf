704ff4aa76543014ff5a777f2ce0fbab4d7a8581
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-06-06 19:23:06 +0800

    fix: restore necessary urls

--- django/ShopZone/urls.py
+++ django/ShopZone/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
+    path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 ]
 
