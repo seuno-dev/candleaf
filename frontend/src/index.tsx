ba5354d7862e8e883c127c49d1508d2e4f2a71b0
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-06-16 14:39:06 +0800

    fix: remove react palette code

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {ThemeProvider} from "@material-tailwind/react";
+import { ThemeProvider } from "@material-tailwind/react";
 import "./index.css";
 import App from "./App";
-import {worker} from "./mocks/browser";
-import {ChakraProvider} from "@chakra-ui/react";
+import { worker } from "./mocks/browser";
+import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
 
-if (process.env.NODE_ENV === "development" && process.env.REACT_APP_SERVER === "msw") {
+if (
+  process.env.NODE_ENV === "development" &&
+  process.env.REACT_APP_SERVER === "msw"
+) {
   worker.start();
 }
 
@@ -21,11 +22,7 @@ root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ThemeProvider>
-        <DevSupport ComponentPreviews={ComponentPreviews}
-                    useInitialHook={useInitial}
-        >
-          <App/>
-        </DevSupport>
+        <App />
       </ThemeProvider>
     </ChakraProvider>
   </React.StrictMode>
