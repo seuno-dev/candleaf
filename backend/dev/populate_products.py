c61f0ccd871cb1c162f678ab43bfde670fb6353d
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-06-26 14:45:09 +0800

    feat: update the script to populate products

--- backend/dev/populate_products.py
+++ backend/dev/populate_products.py
@@ -1,14 +1,21 @@
-import itertools
+import sys
 import os
+
+script_path = os.path.dirname(__file__)
+project_dir = os.path.abspath(os.path.join(script_path, '..', '..', 'backend'))
+sys.path.insert(0, project_dir)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'candleaf.settings')
+
+from django.core.files import File
+
+import itertools
 import random
 from io import BytesIO
 
 from django.db import transaction
 from django.db.models.fields import files
 from model_bakery import baker
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'candleaf.settings')
 
 import requests
 import django
@@ -33,36 +40,35 @@ def download_temp_image(url):
     return file_name, files.File(fp)
 
 
-def populate():
-    print("Populating categories...")
-    cat_cache = {}
-    cat_responses = requests.get("https://dummyjson.com/products/categories")
-    for cat_title in json.loads(cat_responses.text):
-        print(cat_title)
-        cat_cache[cat_title] = clean_and_create_category(cat_title)
-
+def populate(products_data):
     print("Populating products...")
-    product_responses = requests.get("https://dummyjson.com/products?limit=100")
-    for product_dict in json.loads(product_responses.text)['products']:
-        print(product_dict['id'])
+    for (index, product_dict) in enumerate(products_data):
+        print(index)
         product = Product.objects.create(title=product_dict['title'], slug=slugify(product_dict['title']),
                                          description=product_dict['description'],
-                                         unit_price=product_dict['price'],
-                                         category=cat_cache[product_dict['category']])
-        for image in product_dict['images']:
-            file_name, file = download_temp_image(image)
-            product_image = ProductImage(product=product)
-            product_image.image.save(file_name, file)
-            product_image.save()
+                                         unit_price=product_dict['unit_price'],
+                                         wax=product_dict['wax'],
+                                         fragrance=product_dict['fragrance'],
+                                         burning_time=product_dict['burning_time'],
+                                         dimension=product_dict['dimension'],
+                                         weight=product_dict['weight'])
+
+        product_image = ProductImage(product=product)
+
+        image_file_name = product_dict['title'] + ".png"
+        image_file = open("./dev/products_data/images/" + image_file_name, 'rb')
+        product_image.image.save(image_file_name, File(image_file))
+        product_image.save()
+        image_file.close()
 
         baker.make(Review, _quantity=random.randint(1, 10), order_item__product=product, rating=itertools.cycle([1, 3]),
                    comment="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis blandit sem tristique, ultricies ex ac, congue ligula.")
-        baker.make(Review, _quantity=random.randint(1, 90), order_item__product=product, rating=itertools.cycle([4, 5]),
+        baker.make(Review, _quantity=random.randint(20, 90), order_item__product=product,
+                   rating=itertools.cycle([4, 5]),
                    comment="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis blandit sem tristique, ultricies ex ac, congue ligula.")
 
 
 def delete_current_data():
-    Category.objects.all().delete()
     OrderItem.objects.all().delete()
     Order.objects.all().delete()
     Product.objects.all().delete()
@@ -72,7 +78,10 @@ def delete_current_data():
 @transaction.atomic
 def main():
     delete_current_data()
-    populate()
+
+    f = open("./dev/products_data/products.json")
+    populate(json.load(f))
+    f.close()
 
 
 if __name__ == '__main__':
