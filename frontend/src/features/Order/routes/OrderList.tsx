943122082ef94917b17a84b550eb78e4211b1dad
Amelia <amelialukis01@gmail.com>
2023-07-01 15:56:27 +0800

    feat: update order card

--- frontend/src/features/Order/routes/OrderList.tsx
+++ frontend/src/features/Order/routes/OrderList.tsx
@@ -1,26 +1,15 @@
 import React, {useEffect, useState} from "react";
 import OrderCard from "../components/OrderCard";
-import { Order } from "../types";
-import OrderDetailDialog from "../components/OrderDetailDialog";
 import OrderFilter from "../components/OrderFilter";
 import {useNavigate, useSearchParams} from "react-router-dom";
 import useOrderList from "../hooks/useOrderList";
 import {Box, Button, useDisclosure} from "@chakra-ui/react";
 
 function OrderList() {
-  const [selectedOrder, setSelectedOrder] = useState<Order | null>(null);
-  const [openDetailDialog, setOpenDetailDialog] = useState(true);
   const [dateFilter, setDateFilter] = useState({orderTimeMin:"", orderTimeMax:""});
   const {data:orders} = useOrderList(dateFilter);
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
-
-  const handleOpenDetailDialog = () => setOpenDetailDialog(!open);
-
-  const handleClickDetail = (order: Order) => {
-    setSelectedOrder(order);
-    setOpenDetailDialog(true);
-  };
 
   const { isOpen, onOpen, onClose } = useDisclosure();
   const handleSubmit = (dateMin:string, dateMax:string) => {
@@ -42,9 +31,9 @@ function OrderList() {
 
 
   return (
-    <Box maxW={{base:"full", md:"container.xl"}} mx={{md:"auto"}} px={{base:"15px", md:0}}>
-      <Box textAlign="right">
-        <Button colorScheme="primary" onClick={onOpen} my="20px">
+    <Box maxW={{base:"full", md:"container.xl"}} mx={{md:"auto"}} px={{base:"15px", md:0}} pb="30px">
+      <Box textAlign="right" mb={{base:"10px", md:0}}>
+        <Button colorScheme="primary" onClick={onOpen}>
           Date Filter
         </Button>
       </Box>
@@ -52,17 +41,10 @@ function OrderList() {
       <ul>
         {orders?.results.map((order) => (
           <li key={order.id} className="mb-2">
-            <OrderCard order={order} handleClickDetail={handleClickDetail} />
+            <OrderCard order={order}/>
           </li>
         ))}
       </ul>
-      {selectedOrder && (
-        <OrderDetailDialog
-          order={selectedOrder}
-          open={openDetailDialog}
-          handler={handleOpenDetailDialog}
-        />
-      )}
     </Box>
   );
 }
