81f221f0e13f4f986dc211aa7ef1bdd757ca827b
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-07-01 17:16:08 +0800

    chore: reformat code

--- frontend/src/components/Elements/__tests__/Navbar.test.tsx
+++ frontend/src/components/Elements/__tests__/Navbar.test.tsx
@@ -6,22 +6,8 @@ import {
 import { render, screen } from "@testing-library/react";
 import Navbar from "../Navbar";
 import userEvent from "@testing-library/user-event";
-import { categories } from "../../../test/server/db/categories";
 import { customer } from "../../../test/server/db/credential";
-
-describe("Categories dropdown", () => {
-  it("should show the correct categories", async () => {
-    render(<Navbar />, { wrapper: createRouterWrapper() });
-
-    await userEvent.click(screen.getByTestId("navbar-category"));
-
-    await screen.findByText(categories[0].title);
-
-    categories.forEach((category) => {
-      expect(screen.getByText(category.title)).toBeDefined();
-    });
-  });
-});
+import React from "react";
 
 describe("Customer dropdown", () => {
   it("should show customer's full name", async () => {
