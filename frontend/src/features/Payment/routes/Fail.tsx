a869694b6ea9f3d59c9f659a2d6091bc737b4297
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-07-01 16:42:08 +0800

    feat: add payment failed page

--- frontend/src/features/Payment/routes/Fail.tsx
+++ frontend/src/features/Payment/routes/Fail.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Box, Container, Heading, Text, VStack } from "@chakra-ui/react";
 
 function Fail() {
   return (
-    <div>
-      <h1>Fail</h1>
-    </div>
+    <Box>
+      <Container maxW="container.xl" py="60px">
+        <VStack spacing="20px">
+          <Heading>Payment Failed</Heading>
+          <Text maxW="xl">
+            Please check your payment method or try again later.
+          </Text>
+        </VStack>
+      </Container>
+    </Box>
   );
 }
 
