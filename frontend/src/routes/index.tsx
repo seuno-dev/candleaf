3b1b7cc4df121b22d69b33a0412cebfd7969534a
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-07-01 08:56:15 +0800

    feat: change the name of payment feature

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -11,7 +11,7 @@ import React from "react";
 import CartRoutes from "../features/Cart/routes";
 import OrderRoutes from "../features/Order/routes";
 import Navbar from "../components/Elements/Navbar";
-import StripePaymentRoutes from "../features/StripePayment/routes";
+import StripePaymentRoutes from "../features/Payment/routes";
 import ProfileRoutes from "../features/Profile/routes";
 import { getAuthenticationStatus } from "../api";
 import { Typography } from "@material-tailwind/react";
