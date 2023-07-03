e306fdfb57c1118ea0476d4b98e86960f6d01a40
Amelia <amelialukis01@gmail.com>
2023-07-03 15:07:12 +0800

    feat: update add review

--- frontend/src/features/Order/components/AddReview.tsx
+++ frontend/src/features/Order/components/AddReview.tsx
@@ -1,50 +1,88 @@
 import React, { useState } from "react";
-import { Button, Option, Select, Textarea } from "@material-tailwind/react";
-import useAddReview from "../hooks/useAddReview";
 import { OrderItem } from "../types";
+import {
+  Box,
+  Button,
+  HStack,
+  Image,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Stack,
+  Text,
+  Textarea
+} from "@chakra-ui/react";
+import {ChevronDownIcon} from "@chakra-ui/icons";
+import useAddReview from "../hooks/useAddReview";
 
 interface Props {
   orderItem: OrderItem;
-  setOrderItem: React.Dispatch<React.SetStateAction<OrderItem>>;
+  // setOrderItem: React.Dispatch<React.SetStateAction<OrderItem>>;
   onSubmit: () => void;
 }
 
-const AddReview = ({ orderItem, setOrderItem, onSubmit }: Props) => {
-  const { mutate } = useAddReview(orderItem, setOrderItem);
+const AddReview = ({ orderItem, onSubmit }: Props) => {
+  const [item, setItem] = useState(orderItem);
+  const { mutate } = useAddReview(item, setItem);
   const [rating, setRating] = useState<number>(5);
   const [comment, setComment] = useState("");
 
   return (
-    <div className="flex flex-col gap-3">
-      <Select
-        value={`${rating}`}
-        onChange={(value) => setRating(parseInt(value || ""))}
-        label="Ratings"
-        color="light-green"
+    <Stack mb="15px">
+      <HStack
+        key={orderItem.id}
+        my="10px"
       >
-        <Option value="1">1 star</Option>
-        <Option value="2">2 stars</Option>
-        <Option value="3">3 stars</Option>
-        <Option value="4">4 stars</Option>
-        <Option value="5">5 stars</Option>
-      </Select>
+        <Image
+          src={orderItem.product.image.image}
+          alt={`Image of ${orderItem.product.title}`}
+          bgColor="#F7F8FA"
+          maxW={{ base: "70px", md: "120px" }}
+        />
+        <Box textAlign="left" ml={{ md: "20px" }}>
+          <Text fontSize={{ base: "lg", md: "xl" }}>
+            {orderItem.product.title}
+          </Text>
+        </Box>
+      </HStack>
+      <Menu>
+        <HStack>
+          <Text>Rating:</Text>
+          <MenuButton
+            px={4}
+            py={2}
+            transition="all 0.2s"
+            borderRadius="md"
+            borderWidth="1px">
+            {rating} star{rating !== 1 && "s"} <ChevronDownIcon />
+          </MenuButton>
+        </HStack>
+        <Text>Review:</Text>
+        <MenuList>
+          <MenuItem onClick={()=>setRating(1)}>1 star</MenuItem>
+          <MenuItem onClick={()=>setRating(2)}>2 stars</MenuItem>
+          <MenuItem onClick={()=>setRating(3)}>3 stars</MenuItem>
+          <MenuItem onClick={()=>setRating(4)}>4 stars</MenuItem>
+          <MenuItem onClick={()=>setRating(5)}>5 stars</MenuItem>
+        </MenuList>
+      </Menu>
       <Textarea
         value={comment}
         onChange={(e) => setComment(e.target.value)}
-        label="Comment"
-        color="light-green"
       />
       <Button
-        color="light-green"
-        fullWidth={true}
+        colorScheme="primary"
         onClick={() => {
           mutate({ orderItem: orderItem.id, rating, comment });
           onSubmit();
+          setRating(5);
+          setComment("");
         }}
       >
         Submit
       </Button>
-    </div>
+    </Stack>
   );
 };
 
